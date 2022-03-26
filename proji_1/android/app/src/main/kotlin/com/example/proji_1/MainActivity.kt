4318b6a641c0f8b442a9b499a3741d8c34aae07b
Your Name <you@example.com>
2022-03-26 09:43:47 +0000

    my first commit

---
+++ proji_1/android/app/src/main/kotlin/com/example/proji_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.proji_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ proji_2/android/app/src/main/kotlin/com/example/proji_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.proji_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
