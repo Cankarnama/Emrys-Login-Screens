6672d6c103a5df971529eb2de71580d4041df38c
Your Name <you@example.com>
2022-03-28 05:41:23 +0000

    Declared methods for repeating features

--- proji_1/lib/Register01.dart
+++ proji_1/lib/Register01.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
 class Register01 extends StatelessWidget {
+
+
   @override
   Widget build(BuildContext context)=>Scaffold(
     appBar: AppBar(
-      //elevation: 0,
+      elevation: 0,
       title: Text(
-              'Step 1 of 2'),
+              'STEP 1 OF 2',
+      style: TextStyle(
+        color: Colors.grey[600],
+            fontSize: 15
+      ), ),
            centerTitle: true,
       backgroundColor: Colors.white,
 
@@ -30,50 +36,93 @@ class Register01 extends StatelessWidget {
 
     ),
 
-  body: SafeArea(
-  child: Container(
-      color: Colors. white,
+  body: Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 18),
+    child: SafeArea(
+    child: Container(
+        color: Colors. white,
 
  child: Column(
-
+     crossAxisAlignment: CrossAxisAlignment.start,
 children:<Widget> [
 
-     Container(
-      margin: EdgeInsets.only(left: 10, top :20),
+       Container(
+        margin: EdgeInsets.only( top :20),
+        child: Text(
+
+            'Join the Emrys Family',
+             style: TextStyle(
+               fontWeight: FontWeight.bold,
+               fontSize: 22,
+               color: Colors.black,
+             ),
+        ),
+      ),
+
+    Container(
+      margin: EdgeInsets.only( top :6),
       child: Text(
+        'Pleas fill in a few details below',
+        style: TextStyle(
 
-          'Join thE Emry\'s Family',
-           style: TextStyle(
-             fontWeight: FontWeight.bold,
-             fontSize: 22,
-             color: Colors.black,
-           ),
+          fontSize: 18,
+          color: Colors.grey[600],
+        ),
       ),
     ),
-  ),
-
-  Container(
-    margin: EdgeInsets.only(left: 10, top :20),
-    child: Text(
-      'Pleas fill in a few details below',
-      style: TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 22,
-        color: Colors.black,
-      ),
-    ),
-  ),
-
-
+   ShortText("howdy"),
+ LongText(hint:'hello every bodayy'),
+  ShortText("howdy"),
+    LongText(hint:'hello every bodayy'),
 
 ],
 
 
-
  )
 
-  )
-  ));}
+    )
+
+    ),
+  ));
+  Widget LongText({required String hint}) =>
+      TextField(
+        textAlign: TextAlign.left,
+        decoration: InputDecoration(
+            hintText: hint,
+          hintStyle: TextStyle(
+           //   fontWeight: FontWeight.bold
+            fontSize: 18
+          ),
+          enabledBorder:  UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.black12),
+          ),
+          contentPadding: const EdgeInsets. only(top: 15.0),
+             ),
+        textInputAction: TextInputAction.done,
+      );}
+
+      Widget ShortText(String shortext)=>
+           Container(
+             margin: EdgeInsets.only( top :12),
+             child: Row(
+children :const <Widget>[
+Text(
+'Name',
+style: TextStyle(
+fontSize: 12,
+color: Colors.black,
+),
+),
+Text(
+'*',
+style: TextStyle(
+fontSize: 12,
+color: Colors.red,
+),
+)
+]
+),
+           );
 
 /*class LoginScreen_5 extends StatelessWidget {
 
