d75fedee413c0ea72993c0dfeb919af0f81af811
Your Name <you@example.com>
2022-03-30 04:26:22 +0000

    field text box with arrow widget

--- proji_1/lib/Register02.dart
+++ proji_1/lib/Register02.dart
@@ -2,11 +2,10 @@ import 'package:flutter/material.dart';
 import 'Register01.dart';
 
 class Register02 extends StatelessWidget {
+  var items = ['Working a lot harder', 'Being a lot smarter'] ;
   @override
-  Widget build(BuildContext context) =>
-      Scaffold(
-
-        appBar: AppBar(
+  Widget build(BuildContext context) => Scaffold(
+      appBar: AppBar(
         elevation: 0,
         title: Text(
           'STEP 1 OF 2',
@@ -31,39 +30,52 @@ class Register02 extends StatelessWidget {
           )
         ],
       ),
-  body: Padding(
-  padding: const EdgeInsets.symmetric(horizontal: 18),
-  child: SafeArea(
-  child: Container(
-  color: Colors.white,
-  child: Column(
-  crossAxisAlignment: CrossAxisAlignment.start,
-  children: <Widget>[
-  Container(
-  margin: EdgeInsets.only(top: 20),
-  child: const Text(
-  'Provide Platform details',
-  style: TextStyle(
-  fontWeight: FontWeight.bold,
-  fontSize: 22,
-  color: Colors.black,
-  ),
-  ),
-  ),
+      body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 18),
+          child: SafeArea(
+              child: Container(
+                  color: Colors.white,
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.only(top: 20),
+                          child: const Text(
+                            'Provide Platform details',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 22,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 6),
+                          child: Text(
+                            'Please fill in a few details below',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey[600],
+                            ),
+                          ),
+                        ),
+                        TextField(
 
-  Container(
-  margin: EdgeInsets.only(top: 6),
-  child: Text(
-  'Please fill in a few details below',
-  style: TextStyle(
-  fontSize: 18,
-  color: Colors.grey[600],
+                          decoration: InputDecoration(
+                            suffixIcon: PopupMenuButton<String>(
+                              icon: const Icon(Icons.arrow_drop_down),
+                              onSelected: (String value) {
 
-  ),
-  ),
-
-  ),
-
-  ])
-  ))) );
+                              },
+                              itemBuilder: (BuildContext context) {
+                                return items
+                                    .map<PopupMenuItem<String>>((String value) {
+                                  return new PopupMenuItem(
+                                      child: new Text(value), value: value);
+                                }).toList();
+                              },
+                            ),
+                          ),
+                        )
+                      ])))));
 }
